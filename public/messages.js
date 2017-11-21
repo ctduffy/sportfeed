402fd5e14f2fa785a1ce4ca2e262434a8cec6189
Clara <cduffy@students.stab.org>
2017-11-20 20:47:31 -0500

    initial commit with most base files

---
+++ public/messages.js
@@ -0,0 +1,76 @@
+var socket = io.connect();
+
+window.addEventListener('load', function(){
+    console.log('first');
+    socket.on('message', function(nickname, message, time){
+        console.log('here');
+        addOne(nickname, message, time);
+    });
+
+    socket.on('membershipChanged', function(members){
+        //console.log(members);
+        $('#memberList').empty();
+        //console.log('#memberList');
+        //console.log(members.length);
+        for(var i = 0; i < members.length; i++){
+            //console.log(members[i]);  
+            if(members[i] != null){ 
+                $('#memberList').append("<li><span class=\"member\"></span>" + members[i] + "</li><br>");
+            }
+        }
+
+
+    });
+    var meta = document.querySelector('meta[name=roomName]');
+    var roomName = meta.content;
+
+    var nickname = prompt('Enter a nickname: ');
+    console.log(nickname);
+
+    if(nickname ===''){
+        nickname = 'Anonymous';
+    }
+
+    socket.emit('join', roomName, nickname, function(messages){
+        addMessages(messages);
+    });
+    
+    var messageForm = document.getElementById('messageForm');//when user tries to send message
+    messageForm.addEventListener('submit', function(event){ 
+
+        updateScroll(); //scrolls to bottom of messages div
+
+        // prevent the page from redirecting
+        event.preventDefault();
+        socket.emit('message', document.getElementById('messageField').value, roomName)
+        document.getElementById('messageField').value='';
+    } , false); 
+}, false);
+
+function addOne(nickname, message, time){
+    //console.log(message);
+    $('#messages').append("<li> <span class=\"username\">" + nickname + ":"+"</span> <span class=\"time\">" + "(" + time + ")"+"</span> <span class=\"text\">" + message+ "</span></li><br>");
+    updateScroll();
+}
+
+function addMessages(messages){
+    //console.log(messages);
+    //var data = JSON.parse(messages);
+    var data=messages;
+    for (var i = 0; i < data.length; i++) {
+    // have to check if this message is already here
+        //add text, name
+        if(data[i].nickname==''){
+            data[i].nickname='Anonymous';
+        }
+        $('#messages').append("<li id=\"" + data[i].id + "\"> <span class=\"username\">" + data[i].nickname + ":"+"</span> <span class=\"time\">" + "(" + data[i].time + ")"+"</span> <span class=\"text\">" + data[i].message + "</span></li><br>");
+    
+    }
+
+}
+
+function updateScroll(){ //taken from: http://stackoverflow.com/questions/18614301/keep-overflow-div-scrolled-to-bottom-unless-user-scrolls-up
+    //scrolls to bottom of all messages
+    var element = document.getElementById("messages-box");
+    element.scrollTop = element.scrollHeight;
+}
---
+++ server.js
@@ -0,0 +1,154 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+
+var io = require('socket.io').listen(server);
+
+var anyDB = require('any-db');
+var conn = anyDB.createConnection('sqlite3://chatroom.db.sqlite');
+
+var engines = require('consolidate');
+app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
+app.set('views', __dirname + '/templates'); // tell Express where to find templates
+
+app.use(express.bodyParser()); //middleware used for POST, see below
+
+// allow requests to JS/CSS files in local public/ directory
+app.use(express.static(__dirname + '/public'));
+
+
+
+var users = [];
+
+io.sockets.on('connection', function(socket){
+
+	socket.on('join', function(roomName, nickname, callback){
+		socket.join(roomName);
+		socket.nickname = nickname;
+		socket.room=roomName;
+		//console.log(socket.room)
+		//console.log(roomName);
+		//console.log(nickname);
+
+		broadcastMembership(roomName);
+
+		var messages = []; //put messages here
+
+		var m = conn.query('SELECT * FROM messages WHERE RoomName = $1',[roomName]);
+		m.on('data', function(row){
+			messages.push(row);
+			
+		});
+		m.on('end', function(){
+			//console.log(messages);
+			callback(messages);
+		});
+		
+	});
+
+	socket.on('nickname', function(nickname){
+		socket.nickname = nickname;
+	});
+
+	socket.on('message', function(message, roomName){
+		//console.log(message);
+		//console.log(roomName);
+		//var rooms = Object.keys(io.sockets.manager.roomClients[socket.id]);
+		//var roomName = (rooms[0] = '') ? rooms[1].substr(1) : rooms[0].substr(1);
+
+		var m = conn.query('INSERT INTO messages (RoomName, nickname, message) VALUES ($1, $2, $3)',[roomName, socket.nickname, message])
+
+		var currentdate = new Date();
+		var time = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+		
+		//console.log(time);
+
+		io.sockets.in(roomName).emit('message', socket.nickname, message, time);
+	});
+
+	socket.on('disconnect', function(){
+		room = socket.room;
+		deleteMember(room, socket.nickname);
+	});
+
+
+});
+function deleteMember(roomName, nickname){
+	var sockets = io.sockets.clients(roomName);
+
+	var nicknames = sockets.map(function(socket){
+		if(socket.nickname!= nickname){
+			return socket.nickname
+		}
+	});
+
+	console.log(nicknames);
+	io.sockets.in(roomName).emit('membershipChanged', nicknames);
+}
+
+function broadcastMembership(roomName){
+	var sockets = io.sockets.clients(roomName);
+
+	var nicknames = sockets.map(function(socket){
+		return socket.nickname
+	});
+
+	console.log(nicknames);
+	io.sockets.in(roomName).emit('membershipChanged', nicknames);
+}
+
+app.get('/', function(request, response){ //homepage
+	var rooms = [];
+	var q = conn.query('SELECT * FROM Rooms');
+	q.on('data', function(row){
+
+		rooms.push({RoomName:row.RoomName});
+	});
+	q.on('end', function(){
+		//console.log(rooms);
+		response.render('index.html',{roomlist: rooms});
+	});
+});
+function generateRoomIdentifier(response) { //creates random name for all new rooms
+	//console.log('new');
+	var name = "";
+	var chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+	for (var i = 0; i < 6; i++){ //for statement to make a new roomname
+		name += chars.charAt(Math.floor(Math.random() * chars.length));
+	}
+
+	var z = conn.query('SELECT * FROM Rooms WHERE RoomName == $1', [name]);
+	z.on('data', function(row){//if the room already exists
+		generateRoomIdentifier(response);
+	});
+	z.on('end', function(){
+		//console.log(result);
+		var x = conn.query('INSERT INTO Rooms VALUES ($1)', [name]);
+		response.redirect('/'+name);
+	});
+};
+app.get('/New', function(request, response){ //if there is a request for a new room, create a random name and redirect the user to the page with that as its name
+	generateRoomIdentifier(response);
+});
+
+app.get('/:roomName', function(request, response){ //finds room and takes user to the page and fills out the room template so that it appears as the correct room
+	var q = conn.query('SELECT * FROM Rooms WHERE RoomName = $1', [request.params.roomName]);
+	q.on('data', function(row){
+
+	});
+	q.on('end', function(){
+		response.render('room.html', {roomName: request.params.roomName});	
+		// this code is executed after all rows have been returned
+	});
+
+	q.on('err', function(){
+		//response.write('there is nothing here');
+		console.log('room requested doesnt exist');
+	});
+
+	var name = request.params.roomName; // 'ABC123' // ...
+});
+
+
+server.listen(8080);
